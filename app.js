98aed47b9e58aed567732086065e66eebdafff5f
dilbwagsingh <dilbwagsinghb.met18@itbhu.ac.in>
2021-03-09 10:55:12 +0530

    Modified msg

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Making progress already with AWS"));
+app.get("/", (req, res) =>
+  res.send(
+    "Making progress already with AWS. Now this is an extra msg added to check manual reviewing option in AWS."
+  )
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
